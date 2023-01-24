757d298c4ad47c1ceca213d5663b4feb74f3fbb5
Hugi Hlynsson <hugihlynsson@gmail.com>
2023-01-24 20:49:45 +0000

    Update web segment.ts and add segment.js

--- javascript/web/segment.js
+++ javascript/web/segment.js
@@ -1,39 +1,36 @@
 // Install the Segment browser library: npm install --save @segment/analytics-next
 import { AnalyticsBrowser } from "@segment/analytics-next"; // Tested with version 1.49.2
 
-// Import the Avo Codegen file from the location you've chosen
-import { CustomDestination } from "../path/to/Avo";
+let analytics;
 
-let analytics: AnalyticsBrowser | undefined;
-
-const segmentDestinationInterface: CustomDestination = {
+const segmentDestinationInterface = {
   make: (_env, apiKey) => {
     analytics = AnalyticsBrowser.load({ writeKey: apiKey });
   },
 
-  identify: (userId) => analytics?.identify(userId),
+  identify: (userId) => analytics.identify(userId),
 
   logEvent: (eventName, eventProperties) =>
-    analytics?.track(eventName, eventProperties),
+    analytics.track(eventName, eventProperties),
 
-  setUserProperties: (userProperties) => analytics?.identify(userProperties),
+  setUserProperties: (userProperties) => analytics.identify(userProperties),
 
-  unidentify: () => analytics?.reset(),
+  unidentify: () => analytics.reset(),
 
   logPage: (eventName, eventProperties) =>
-    analytics?.page(eventName, eventProperties),
+    analytics.page(eventName, eventProperties),
 
   revenue: (amount, eventProperties) =>
-    analytics?.track("Purchase Complete", {
+    analytics.track("Purchase Complete", {
       ...eventProperties,
       revenue: amount,
     }),
 
   // The following methods are used for group analytics and are not required. Learn more about group analytics here: https://www.avo.app/docs/data-design/groups
   setGroupProperties: (_groupType, groupId, groupProperties) =>
-    analytics?.group(groupId, groupProperties),
+    analytics.group(groupId, groupProperties),
 
-  addCurrentUserToGroup: (_groupType, groupId) => analytics?.group(groupId),
+  addCurrentUserToGroup: (_groupType, groupId) => analytics.group(groupId),
 
   // Not supported by the Segment SDK
   // logEventWithGroups: (eventName, eventProperties, groupTypesToGroupIds) => {},
