7ee02db3b73f22ec9f197fabeac35a9cbae752c1
Hugi Hlynsson <hugihlynsson@gmail.com>
2023-01-24 21:21:30 +0000

    Add more context to freshpaint.ts and add freshpaint.js

--- javascript/web/freshpaint.js
+++ javascript/web/freshpaint.js
@@ -1,11 +1,6 @@
 // Install the freshpaint SDK: https://documentation.freshpaint.io/readme/guides/quickstart/installing-freshpaint
 
-// Import the Avo Codegen file from the location you've chosen
-import { CustomDestination } from "../path/to/Avo";
-
-// Get the Freshpaint types from here: https://documentation.freshpaint.io/readme/guides/quickstart/how-do-i-install-freshpaint-with-typescript
-
-const freshpaintDestinationInterface: CustomDestination = {
+const freshpaintDestinationInterface = {
   make: (_env, _apiKey) => {
     // Required if you haven't initialized the Freshpaint SDK in your app already
     freshpaint.init();
@@ -15,19 +10,16 @@ const freshpaintDestinationInterface: CustomDestination = {
   identify: (userId) => freshpaint.identify(userId),
 
   logEvent: (eventName, eventProperties) =>
-    freshpaint.track(eventName, eventProperties as Record<string, unknown>),
+    freshpaint.track(eventName, eventProperties),
 
   setUserProperties: function (userId, userProperties) {
-    freshpaint.identify(userId, userProperties as Record<string, unknown>);
+    freshpaint.identify(userId, userProperties);
   },
 
   unidentify: () => freshpaint.reset(),
 
   logPage: (pageName, eventProperties) =>
-    freshpaint.page({ ...eventProperties, name: pageName } as Record<
-      string,
-      unknown
-    >),
+    freshpaint.page({ ...eventProperties, name: pageName }),
 
   // Revenue is not supported by Freshpaint
   revenue: (_amount, _eventProperties) =>
