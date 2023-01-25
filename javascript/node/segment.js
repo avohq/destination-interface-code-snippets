9ecf422f8c98258d0e327ef743e9133c1745d081
Hugi Hlynsson <hugihlynsson@gmail.com>
2023-01-25 12:20:24 +0000

    Update Node Segment TS and add JS

--- javascript/node/segment.js
+++ javascript/node/segment.js
@@ -1,31 +1,26 @@
 // Install the official Segment Node.js SDK: npm i --save analytics-node
 import Analytics from "analytics-node"; // Tested with version 6.2.0
 
-// Get types from the DefenitelyTyped library: npm install --save-dev @types/analytics-node
+let analytics;
 
-// Import the Avo Codegen file from the location you've chosen
-import { CustomDestination } from "../path/to/Avo";
-
-let analytics: Analytics | undefined;
-
-const segmentDestinationInterface: CustomDestination = {
+const segmentDestinationInterface = {
   make: (_env, apiKey) => {
     analytics = new Analytics(apiKey, { flushAt: 1 });
   },
 
-  identify: (userId) => analytics?.identify({ userId }),
+  identify: (userId) => analytics.identify({ userId }),
 
   logEvent: (userId, eventName, eventProperties) =>
-    analytics?.track({ userId, event: eventName, properties: eventProperties }),
+    analytics.track({ userId, event: eventName, properties: eventProperties }),
 
   setUserProperties: (userId, userProperties) =>
-    analytics?.identify({ userId, traits: userProperties }),
+    analytics.identify({ userId, traits: userProperties }),
 
   logPage: (userId, pageName, eventProperties) =>
-    analytics?.page({ userId, name: pageName, properties: eventProperties }),
+    analytics.page({ userId, name: pageName, properties: eventProperties }),
 
   revenue: (userId, amount, eventProperties) =>
-    analytics?.track({
+    analytics.track({
       userId,
       event: "Revenue",
       properties: { ...eventProperties, amount },
@@ -33,10 +28,10 @@ const segmentDestinationInterface: CustomDestination = {
 
   // The following methods are used for group analytics and you might not need them. Learn more about group analytics here: https://www.avo.app/docs/data-design/groups
   addCurrentUserToGroup: (userId, _groupType, groupId) =>
-    analytics?.group({ userId, groupId }),
+    analytics.group({ userId, groupId }),
 
   setGroupProperties: (userId, _groupType, groupId, groupProperties) =>
-    analytics?.group({ userId, groupId, traits: groupProperties }),
+    analytics.group({ userId, groupId, traits: groupProperties }),
 
   // Not supported by the Segment SDK
   // logEventWithGroups: (eventName, eventProperties, groupTypesToGroupIds) => {},
