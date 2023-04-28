501a69e89c32b47a072e02c02dd3046bd5ea0ef8
Sölvi Logason <logason@gmail.com>
2023-04-28 17:30:06 +0000

    Google Analytics 3 on JS Web

---
+++ javascript/web/GoogleAnalytics3.js
@@ -0,0 +1,34 @@
+import Avo from './Avo';
+
+// An object representing your Google Analytics destination that you pass into initAvo():
+
+var googleAnalyticsDestinationInterface = {
+  logEvent: function (eventName, eventProperties) {
+    gtag('event', eventName, eventProperties);
+    this.dataLayer.push({
+      ...eventProperties
+      event: properties.event || 'interaction',
+      category: properties.category || 'Event',
+      action: properties.action,
+      label: properties.label,
+      value: properties.value,
+      interactionType: properties.noninteraction,
+      userId: ... // Todo pass in userId from your environment,
+   });
+  },
+  setUserProperties: function (userId, userProperties) {
+    // Todo: Pass user properties to Google Analytics
+  },
+  identify: function (userId) {
+    // Todo: Identify user in Google Analytics
+  },
+  unidentify: function () {
+    // Todo: Unidentify currently identified user in Google Analytics
+  },
+  revenue: function (amount, eventProperties) {
+    // Todo: Log revenue in Google Analytics
+  },
+  page: function (screenName, eventProperties) {
+    // Todo: Log page view in Google Analytics
+  },
+};
